44cf727dca69f0cd1957a4418effe446087add40
albertjacobsz <dekater403@gmail.com>
2021-05-04 00:31:34 +0400

    created the ability for the user to search up a specific subject title

--- main.cpp
+++ main.cpp
@@ -12,10 +12,12 @@ string charArr2String(char*);
 Node init_subject();
 string userInput();
 void startPrint();
+string toLowerCase(string);
 //END PROTOTYPE DECLARATIONS
 
 int main(){
     startPrint();
+    //get user input to continue to the query
     int cont;
     while(cin>>cont || cont != 1){
         if(cont == 1){
@@ -23,11 +25,51 @@ int main(){
         }
         cout<< "please enter in one" << endl;
     }
-    
+    //get query input
+    string query = userInput();
+    cout << "Searching for " << query << endl;
+    //initialize all the data for the query
+    Search search;
     Subject info[81];
     Subject *p = info;
     p = init_data();
+    //Create new Link list
+    Node *list = new Node();
+    //check if the the Link list exists
+    bool outer = false;
+    for(int i = 45; i<56; i++){
+        //get subject at specific i index
+        Subject subject = p[i];
+        //turn the title into a string
+        string data = charArr2String(p[i].subTitle);
+        //see whether pattern occurs in the string
+        bool yn = search.searchPattern(query,data);
+        //if its true
+        if(yn){
+            //add the specific subject to the link list
+           list = list->push(list, subject);
+           //ensure that we know the link list has objects inside of it
+           outer = true;
+        }
+        
+    }
+    //if we knoow the link list has objects inside of it
+    if(outer == true){
+        //reverse the link list, since the link list's current pointer is at the end of the link list
+        list = list->reverse(list);
+        //print out all the titles
+        while(list->next != NULL){
+            cout << list->getData().subTitle <<endl;
+            //and move on the next node
+            list = list->next;
+        }
+        //since while loop exits at the last node (without printing it) we print the data of the last node
+        cout << list->getData().subTitle <<endl;
+    }
+    
+
     //test
+    /*
     for (int i = 45; i<56;i++){
         cout << setw(15) << "Level: " << p[i].level << endl;
         cout << setw(15) << "Code: " << p[i].subCode << endl;
@@ -39,7 +81,7 @@ int main(){
         cout << setw(15) << "lecture: " << p[i].lecture << endl;
         cout << endl;
     }
-
+    */
  return 0;
 }
 
@@ -52,21 +94,21 @@ string charArr2String(char* array){
     }
     return sentence;
 }
+string toLowerCase(string text){
+
+}
 void startPrint(){
     cout << "Welcome to the COMP database" << endl;
     cout << "In this program, you can find the information about all the subjects offered by the COMP department" << endl;
     cout << "To search for a subject enter 1" << endl;
 }
-Node init_subject(){
-    Node *list = new Node();
-
-    //Add subjects to the list using the Doubly Link list functions
-}
 string userInput(){
     // todo add data verification
-    string user;
+    char user[160];
     cout<< "Please enter in a key word or phrase" << endl;
-    getline(cin,user);
-    return user;
+    cin >> user;
+    string userIn;
+    userIn = charArr2String(user);
+    return userIn;
 }
 
