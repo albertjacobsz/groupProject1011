4ccad6fcd7bdaf65e2880b75b791fc8c09751e73
albertjacobsz <dekater403@gmail.com>
2021-05-06 01:41:58 +0400

    added lowerCase function and enabled searching once more

--- Search.h
+++ Search.h
@@ -1,62 +1,54 @@
-/*
-
-SearchPattern function was created using the inspiration of https://www.youtube.com/watch?v=ZMQWjslBlbU Code kits's psuedocode
-removeSpaces function was created using the inspiration of https://www.geeksforgeeks.org/remove-spaces-from-a-given-string/ Code
-*/
-
-//todo change functionality
-#include <string.h>
 #include <string>
+#include <iostream>
 class Search{
     private:
         //void badCharHeuristic(string, int[MAX_CHAR_LENGTH]);
-        int min(int, int);
-        int last(std::string, int, std::string);
-        std::string removeSpaces(std::string);
+        std::string toLowerCase(std::string);
         
     public:
         bool searchPattern(std::string,std::string);
 };
-int Search::min(int j, int i){
-    if(j < i){
-        return j;
-    }else if(j > i){
-        return i;
-    }else{
-        return j;
-    }
-}
-int Search::last(std::string text, int i, std::string query){
-    int size = query.size();
 
-    for(int j = size; j>=0; j--){
-        if(text[i] == query[j]){
-            return j;
-        }
-    }
-    return -1;
-}
 bool Search::searchPattern(std::string query, std::string txt){
-    int n = txt.size();
-    int i = query.size()-1;
-    int j = query.size() - 1;
-    while(!(i > n-1)){
+    query = toLowerCase(query);
+    txt = toLowerCase(txt);
+    int sq = query.size();
+    int st = txt.size();
+    int i = 0;
+    int j = 0;
+    while(i < st){
         if(query[j] == txt[i]){
-            if(j == 0){
+            if(j == sq-1){
                 return true;
-            }else{
-                i--;
-                j--;
+            }
+            if(i != st){
+                i++;
+                j++;
             }
         }else{
-            i = i + query.size() - min(j,1+last(txt,i,query));
-            j = query.size()-1;
+            i++;
+            j = 0;
         }
     }
     return false;
 }
-/*std::string Search::removeSpaces(std::string str){
-    str.erase(remove(str.begin(), str.end(), ' '), str.end());
-    return str;
+std::string Search::toLowerCase(std::string text){
+    int size = text.size();
+    std::string newString = "";
+    for(int i = 0; i<size; i++){
+        char temp = text[i];
+        if((int(temp) >= 65) && (int(temp) <= 90)){
+           // std::cout<< " INSIDE = "<< std::endl;
+            int off = int(temp) - 65;
+            //std::cout<< " OFF = "<< off << std::endl;
+            off = off + 97;
+            //std::cout<< " NEW OFF = "<<  off << std::endl;
+            temp = off;
+            //std::cout<< " new  = "<< std::endl;
+        }
+        //std::cout<< "new char = " << int(temp) << std::endl;
+        newString += temp;
+    }
+    
+    return newString;
 }
-*/
