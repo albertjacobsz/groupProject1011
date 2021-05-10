06ea71212f732d6e437b2727d9a8e9a4837ef203
asterix75 <33675629+asterix75@users.noreply.github.com>
2021-05-10 20:47:09 +0800

    commit

--- Subject.h
+++ Subject.h
@@ -4,28 +4,531 @@
 using namespace std;
 #pragma once
 struct Subject{
-    int level;//subject level
-    char subCode[20];//
-    char subTitle[70];
-    int credit;
-    int weight;
-    char preReq[5][10];
-    int lecture;
-    int tls;
-    int ca;
-    int exam;
+    int level = 0;//subject level
+    char subCode[20] ="";//
+    char subTitle[70] = "";
+    int credit = 0;
+    int weight = 0;
+    char preReq[5][50];
+    int lecture = 0;
+    int tls = 0;
+    int ca = 0;
+    int exam = 0;
 };
 
 Subject* init_data(){
-    Subject infor[81];//CS for Computer Science
+    Subject infor[90];//CS for Computer Science
     Subject *info = infor;
 
     //make sure the array of preReq is not empty and lost arrays
-    for (int i = 0; i<81;i++){
+    for (int i = 0; i<82;i++){
         for (int j = 0; j < 5; j++){
             strcpy(info[i].preReq[j],"");
         }
     }
+    //0 - Intialize COMP1001
+    info[0].level = 1;
+    strcpy(info[0].subCode,"comp1001");
+    strcpy(info[0].subTitle, "Problem Solving Methodology in Information Technology");
+    info[0].credit = 3;
+    info[0].weight = 3;
+    strcpy(info[0].preReq[0],"nil");
+    info[0].lecture = 26;
+    info[0].tls = 55;
+    info[0].exam = 45;
+    
+    //1 - Intialize COMP1002
+    info[1].level = 1;
+    strcpy(info[1].subCode,"comp1002");
+    strcpy(info[1].subTitle, "computational thinking and problem solving");
+    info[1].credit = 3;
+    info[1].weight = 3;
+    strcpy(info[1].preReq[0],"nil");
+    info[1].lecture = 26;
+    info[1].tls = 55;
+    info[1].exam = 45;
+    
+    //2 - Intialize COMP1003
+    info[2].level = 1;
+    strcpy(info[2].subCode,"comp1003");
+    strcpy(info[2].subTitle, "statistical tools and applications");
+    info[2].credit = 3;
+    info[2].weight = 3;
+    strcpy(info[2].preReq[0],"nil");
+    info[2].lecture = 26;
+    info[2].tls = 55;
+    info[2].exam = 45;
+    
+     //3 - Intialize COMP1011
+    info[3] .level = 1;
+    strcpy(info[3].subCode,"comp1011");
+    strcpy(info[3].subTitle, "programming fundamentals");
+    info[3].credit = 3;
+    info[3].weight = 3;
+    strcpy(info[3].preReq[0],"nil");
+    info[3].lecture = 26;
+    info[3].tls = 55;
+    info[3].exam = 45;
+    
+     //4 - Intialize COMP1411
+    info[4].level = 1;
+    strcpy(info[4].subCode,"comp1411");
+    strcpy(info[4].subTitle, "introduction to computer systems");
+    info[4].credit = 3;
+    info[4].weight = 3;
+    strcpy(info[4].preReq[0],"nil");
+    info[4].lecture = 26;
+    info[4].tls = 55;
+    info[4].exam = 45;
+    
+     //5 - Intialize COMP1433
+    info[5].level = 1;
+    strcpy(info[5].subCode,"comp1433");
+    strcpy(info[5].subTitle, "introduction to data analytics");
+    info[5].credit = 3;
+    info[5].weight = 3;
+    strcpy(info[5].preReq[0],"nil");
+    info[5].lecture = 26;
+    info[5].tls = 55;
+    info[5].exam = 45;
+    
+     //6 - Intialize COMP1901
+    info[6].level = 1;
+    strcpy(info[6].subCode,"comp1901");
+    strcpy(info[6].subTitle, "seminars and topics in information technology");
+    info[6].credit = 3;
+    info[6].weight = 3;
+    strcpy(info[6].preReq[0],"nil");
+    info[6].lecture = 26;
+    info[6].tls = 55;
+    info[6].exam = 45;
+    
+     //7 - Intialize COMP1B02
+    info[7].level = 1;
+    strcpy(info[7].subCode,"comp1b02");
+    strcpy(info[7].subTitle, "friends, followers and circles: connections in a socially-computed world");
+    info[7].credit = 3;
+    info[7].weight = 3;
+    strcpy(info[7].preReq[0],"nil");
+    info[7].lecture = 26;
+    info[7].tls = 55;
+    info[7].exam = 45;
+    
+     //8 - Intialize COMP1B03
+    info[8].level = 1;
+    strcpy(info[8].subCode,"comp1b03");
+    strcpy(info[8].subTitle, "innovation and entrepreneurship: the rise of the ICT industry in china");
+    info[8].credit = 3;
+    info[8].weight = 3;
+    strcpy(info[8].preReq[0],"nil");;
+    info[8].lecture = 26;
+    info[8].tls = 55;
+    info[8].exam = 45;
+    
+    //9 - Intialize COMP1D04
+    info[9].level = 1;
+    strcpy(info[9].subCode,"comp1d04");
+    strcpy(info[9].subTitle, "from scratch to apps: foundations of computational thinking and literacy for problem solving");
+    info[9].credit = 3;
+    info[9].weight = 3;
+    strcpy(info[9].preReq[0],"nil");
+    info[9].lecture = 26;
+    info[9].tls = 55;
+    info[9].exam = 45;
+    
+    //10 - Intialize COMP2011
+    info[10].level = 2;
+    strcpy(info[10].subCode,"comp2011");
+    strcpy(info[10].subTitle, "data structures");
+    info[10].credit = 3;
+    info[10].weight = 3;
+    strcpy(info[10].preReq[0],"comp1011");
+    info[10].lecture = 26;
+    info[10].tls = 55;
+    info[10].exam = 45;
+    
+    //11 - Intialize COMP2012
+    info[11].level = 2;
+    strcpy(info[11].subCode,"comp2012");
+    strcpy(info[11].subTitle, "discrete structures");
+    info[11].credit = 3;
+    info[11].weight = 3;
+    strcpy(info[11].preReq[0],"nil");
+    info[11].lecture = 26;
+    info[11].tls = 55;
+    info[11].exam = 45;
+    
+    //12 - Intialize COMP2021
+    info[12].level = 2;
+    strcpy(info[12].subCode,"comp2021");
+    strcpy(info[12].subTitle, "object-oriented programming");
+    info[12].credit = 3;
+    info[12].weight = 3;
+    strcpy(info[12].preReq[0],"comp1011");
+    info[12].lecture = 26;
+    info[12].tls = 55;
+    info[12].exam = 45;
+    
+    //13 - Intialize COMP2022
+    info[13].level = 2;
+    strcpy(info[13].subCode,"comp2021");
+    strcpy(info[13].subTitle, "programming for finTech applications");
+    info[13].credit = 3;
+    info[13].weight = 3;
+    strcpy(info[13].preReq[0],"comp1001");
+    info[13].lecture = 26;
+    info[13].tls = 55;
+    info[13].exam = 45;
+    
+    //14 - Intialize COMP2121
+    info[14].level = 2;
+    strcpy(info[14].subCode,"comp2121");
+    strcpy(info[14].subTitle, "e-business");
+    info[14].credit = 3;
+    info[14].weight = 3;
+    strcpy(info[14].preReq[0],"nil");
+    info[14].lecture = 26;
+    info[14].tls = 55;
+    info[14].exam = 45;
+    
+    //15 - Intialize COMP2222
+    info[15].level = 2;
+    strcpy(info[15].subCode,"comp2222");
+    strcpy(info[15].subTitle, "introduction to human-computer interaction methods");
+    info[15].credit = 3;
+    info[15].weight = 3;
+    strcpy(info[15].preReq[0],"nil");
+    info[15].lecture = 26;
+    info[15].tls = 55;
+    info[15].exam = 45;
+    
+    //16 - Intialize COMP2322
+    info[16].level = 2;
+    strcpy(info[16].subCode,"comp2322");
+    strcpy(info[16].subTitle, "computer networking");
+    info[16].credit = 3;
+    info[16].weight = 3;
+    strcpy(info[16].preReq[0],"comp1011");
+    info[16].lecture = 26;
+    info[16].tls = 55;
+    info[16].exam = 45;
+    
+     //17 - Intialize COMP2411
+    info[17].level = 2;
+    strcpy(info[17].subCode,"comp2411");
+    strcpy(info[17].subTitle, "database systems");
+    info[17].credit = 3;
+    info[17].weight = 3;
+    strcpy(info[17].preReq[0],"comp1011");
+    info[17].lecture = 26;
+    info[17].tls = 55;
+    info[17].exam = 45;
+    
+     //18 - Intialize COMP2421
+    info[18].level = 2;
+    strcpy(info[18].subCode,"comp2421");
+    strcpy(info[18].subTitle, "computer organization");
+    info[18].credit = 3;
+    info[18].weight = 3;
+    strcpy(info[18].preReq[0],"nil");
+    info[18].lecture = 26;
+    info[18].tls = 55;
+    info[18].exam = 45;
+    
+     //19 - Intialize COMP2422
+    info[19].level = 2;
+    strcpy(info[19].subCode,"comp2422");
+    strcpy(info[19].subTitle, "visual interface and interaction development");
+    info[19].credit = 3;
+    info[19].weight = 3;
+    strcpy(info[19].preReq[0],"comp2011");
+    strcpy(info[19].preReq[0],"comp2021");
+    info[19].lecture = 26;
+    info[19].tls = 55;
+    info[19].exam = 45;
+    
+     //20 - Intialize COMP2432
+    info[20].level = 2;
+    strcpy(info[20].subCode,"comp2422");
+    strcpy(info[20].subTitle, "operating systems");
+    info[20].credit = 3;
+    info[20].weight = 3;
+    strcpy(info[20].preReq[0],"comp2011");
+    info[20].lecture = 26;
+    info[20].tls = 55;
+    info[20].exam = 45;
+    
+    //21 - Intialize COMP2A01
+    info[21].level = 2;
+    strcpy(info[21].subCode,"comp2a01");
+    strcpy(info[21].subTitle, "whose data is it anyway?information and internet ethics, privacy and security");
+    info[21].credit = 3;
+    info[21].weight = 3;
+    strcpy(info[21].preReq[0],"comp2011");
+    info[21].lecture = 26;
+    info[21].tls = 50;
+    info[21].exam = 50;
+    
+    //22 - Intialize COMP2S01
+    info[21].level = 2;
+    strcpy(info[21].subCode,"comp2s01");
+    strcpy(info[21].subTitle, "technology beyond borders:service learning across cultural, ethnic and community lines");
+    info[21].credit = 3;
+    info[21].weight = 3;
+    strcpy(info[21].preReq[0],"nil");
+    info[21].lecture = 26;
+    info[21].tls = 55;
+    info[21].exam = 45;
+    
+    //23 - Intialize COMP3011
+    info[23].level = 3;
+    strcpy(info[23].subCode,"comp3011");
+    strcpy(info[23].subTitle, "design and analysis of algorithms");
+    info[23].credit = 3;
+    info[23].weight = 3;
+    strcpy(info[23].preReq[0],"comp1011");
+    strcpy(info[23].preReq[1],"comp2011");
+    info[23].lecture = 26;
+    info[23].tls = 60;
+    info[23].exam = 40;
+    
+    //24 - Intialize COMP3021
+    info[24].level = 3;
+    strcpy(info[24].subCode,"comp3021");
+    strcpy(info[24].subTitle, "programming language paradigms");
+    info[24].credit = 3;
+    info[24].weight = 3;
+    strcpy(info[24].preReq[0],"comp1011");
+    strcpy(info[24].preReq[1],"comp2011");
+    info[24].lecture = 26;
+    info[24].tls = 55;
+    info[24].exam = 45;
+    
+     //25 - Intialize COMP3121
+    info[25].level = 3;
+    strcpy(info[25].subCode,"comp3121");
+    strcpy(info[25].subTitle, "social and collaborative computing");
+    info[25].credit = 3;
+    info[25].weight = 3;
+    strcpy(info[25].preReq[0],"nil");
+    info[25].lecture = 26;
+    info[25].tls = 55;
+    info[25].exam = 45;
+    
+     //26 - Intialize COMP3122
+    info[26].level = 3;
+    strcpy(info[26].subCode,"comp3122");
+    strcpy(info[26].subTitle, "information systems development");
+    info[26].credit = 3;
+    info[26].weight = 3;
+    strcpy(info[26].preReq[0],"nil");
+    info[26].lecture = 39;
+    info[26].tls = 70;
+    info[26].exam = 30;
+    
+    //27 - Intialize COMP3131
+    info[27].level = 3;
+    strcpy(info[27].subCode,"comp3131");
+    strcpy(info[27].subTitle, "business and information systems strategies");
+    info[27].credit = 3;
+    info[27].weight = 3;
+    strcpy(info[27].preReq[0],"nil");
+    info[27].lecture = 39;
+    info[27].tls = 60;
+    info[27].exam = 40;
+    
+     //28 - Intialize COMP3133
+    info[28].level = 3;
+    strcpy(info[28].subCode,"comp3133");
+    strcpy(info[28].subTitle, "chinese language computing");
+    info[28].credit = 3;
+    info[28].weight = 3;
+    strcpy(info[28].preReq[0],"comp1011");
+    info[28].lecture = 26;
+    info[28].tls = 55;
+    info[28].exam = 45;
+    
+    //29 - Intialize COMP3134
+    info[29].level = 3;
+    strcpy(info[29].subCode,"comp3134");
+    strcpy(info[29].subTitle, "business intelligence and customer relationship management");
+    info[29].credit = 3;
+    info[29].weight = 3;
+    strcpy(info[29].preReq[0],"nil");
+    info[29].lecture = 39;
+    info[29].tls = 55;
+    info[29].exam = 45;
+    
+    //30 - Intialize COMP3211
+    info[30].level = 3;
+    strcpy(info[30].subCode,"comp3211");
+    strcpy(info[30].subTitle, "software engineering");
+    info[30].credit = 3;
+    info[30].weight = 3;
+    strcpy(info[30].preReq[0],"comp1011");
+    strcpy(info[30].preReq[0],"comp2011");
+    info[30].lecture = 39;
+    info[30].tls = 60;
+    info[30].exam = 40;
+    
+     //31 - Intialize COMP3222
+    info[31].level = 3;
+    strcpy(info[31].subCode,"comp3222");
+    strcpy(info[31].subTitle, "software design principles");
+    info[31].credit = 3;
+    info[31].weight = 3;
+    strcpy(info[31].preReq[0],"comp2011");
+    strcpy(info[31].preReq[0],"comp2021");
+    info[31].lecture = 39;
+    info[31].tls = 55;
+    info[31].exam = 45;
+    
+     //32 - Intialize COMP3233
+    info[32].level = 3;
+    strcpy(info[32].subCode,"comp3233");
+    strcpy(info[32].subTitle, "software testing and quality assurance");
+    info[32].credit = 3;
+    info[32].weight = 3;
+    strcpy(info[32].preReq[0],"comp3211");
+    info[32].lecture = 39;
+    info[32].tls = 55;
+    info[32].exam = 45;
+    
+     //33 - Intialize COMP3235
+    info[33].level = 3;
+    strcpy(info[33].subCode,"comp3235");
+    strcpy(info[33].subTitle, "software project management");
+    info[33].credit = 3;
+    info[33].weight = 3;
+    strcpy(info[33].preReq[0],"comp3211");
+    info[33].lecture = 39;
+    info[33].tls = 55;
+    info[33].exam = 45;
+    
+     //34 - Intialize COMP3334
+    info[34].level = 3;
+    strcpy(info[34].subCode,"comp3334");
+    strcpy(info[34].subTitle, "computer systems security");
+    info[34].credit = 3;
+    info[34].weight = 3;
+    strcpy(info[34].preReq[0],"nil");
+    info[34].lecture = 39;
+    info[34].tls = 60;
+    info[34].exam = 40;
+    
+    //35 - Intialize COMP3335
+    info[35].level = 3;
+    strcpy(info[35].subCode,"comp3335");
+    strcpy(info[35].subTitle, "database security");
+    info[35].credit = 3;
+    info[35].weight = 3;
+    strcpy(info[35].preReq[0],"comp2411");
+    info[35].lecture = 39;
+    info[35].tls = 55;
+    info[35].exam = 45;
+    
+    //36 - Intialize COMP3421
+    info[36].level = 3;
+    strcpy(info[36].subCode,"comp3421");
+    strcpy(info[36].subTitle, "web application design and development");
+    info[36].credit = 3;
+    info[36].weight = 3;
+    strcpy(info[36].preReq[0],"comp1011");
+    info[36].lecture = 26;
+    info[36].tls = 55;
+    info[36].exam = 45;
+    
+    //37 - Intialize COMP3422
+    info[37].level = 3;
+    strcpy(info[37].subCode,"comp3422");
+    strcpy(info[37].subTitle, "creative digital media design");
+    info[37].credit = 3;
+    info[37].weight = 3;
+    strcpy(info[37].preReq[0],"comp2011");
+    info[37].lecture = 26;
+    info[37].tls = 55;
+    info[37].exam = 45;
+    
+     //38 - Intialize COMP3423
+    info[38].level = 3;
+    strcpy(info[38].subCode,"comp3423");
+    strcpy(info[38].subTitle, "human computer interaction");
+    info[38].credit = 3;
+    info[38].weight = 3;
+    strcpy(info[38].preReq[0],"comp1011");
+    info[38].lecture = 39;
+    info[38].tls = 55;
+    info[38].exam = 45;
+    
+     //39 - Intialize COMP3432
+    info[39].level = 3;
+    strcpy(info[39].subCode,"comp3432");
+    strcpy(info[39].subTitle, "innovative computing paradigms");
+    info[39].credit = 3;
+    info[39].weight = 3;
+    strcpy(info[39].preReq[0],"comp2011");
+    strcpy(info[39].preReq[0],"comp2021");
+    info[39].lecture = 39;
+    info[39].tls = 55;
+    info[39].exam = 45;
+    
+     //40 - Intialize COMP3438
+    info[40].level = 3;
+    strcpy(info[40].subCode,"comp3438");
+    strcpy(info[40].subTitle, "system programming");
+    info[40].credit = 3;
+    info[40].weight = 3;
+    strcpy(info[40].preReq[0],"comp1011");
+    strcpy(info[40].preReq[0],"comp2432");
+    info[40].lecture = 39;
+    info[40].tls = 55;
+    info[40].exam = 45;
+    
+    //41 - Intialize COMP3511
+    info[41].level = 3;
+    strcpy(info[41].subCode,"comp3511");
+    strcpy(info[41].subTitle, "legal aspects and ethics of computing");
+    info[41].credit = 3;
+    info[41].weight = 3;
+    strcpy(info[41].preReq[0],"nil");
+    info[41].lecture = 26;
+    info[41].tls = 100;
+    info[41].exam = 0;
+    
+    //42 - Intialize COMP3512
+    info[42].level = 3;
+    strcpy(info[42].subCode,"comp3512");
+    strcpy(info[42].subTitle, "legal aspects, professionalism and ethics of computing");
+    info[42].credit = 3;
+    info[42].weight = 3;
+    strcpy(info[42].preReq[0],"nil");
+    info[42].lecture = 39;
+    info[42].tls = 100;
+    info[42].exam = 0;
+    
+    //43 - Intialize COMP3531
+    info[43].level = 3;
+    strcpy(info[43].subCode,"comp3531");
+    strcpy(info[43].subTitle, "IT entrepreneurship");
+    info[43].credit = 3;
+    info[43].weight = 3;
+    strcpy(info[43].preReq[0],"nil");
+    info[43].lecture = 39;
+    info[43].tls = 65;
+    info[43].exam = 35;
+    
+    //44 - Intialize COMP3911
+    info[44].level = 3;
+    strcpy(info[44].subCode,"comp3911");
+    strcpy(info[44].subTitle, "service learning and civic engagement in the information age");
+    info[44].credit = 3;
+    info[44].weight = 3;
+    strcpy(info[44].preReq[0],"nil");
+    info[44].lecture = 28;
+    info[44].tls = 70;
+    info[44].exam = 30;
+
+
     //45 - initialize COMP3921-23:
     info[45].level = 3;
     strcpy(info[45].subCode,"COMP3921-23");
@@ -473,8 +976,5 @@ Subject* init_data(){
     info[80].tls = 378;
     info[80].ca = 100;
     info[80].exam = 0;
-
-
-
     return info;
 }
--- main.cpp
+++ main.cpp
@@ -17,15 +17,109 @@ void startPrint();
 int dataValidation(int);
 string dataValidation(string);
 void printData(Node*);
+void searchSub();//search title
+void searchCode();
+void searchPre();
 //END PROTOTYPE DECLARATIONS
 
 int main(){
     startPrint();
     //get user input to continue to the query
+    /*
     int cont;
-    int found = 0;
     cont = dataValidation(cont);
-    //END TODO
+    */
+    //int found = 0;
+    int choice;
+    cin >> choice;
+    switch(choice){
+    case 1: 
+        searchSub();
+    break;
+    case 2:
+        searchCode();//search subject code
+    break;
+    case 3:
+        searchPre();//search Pre Requisite
+    default:
+    break;
+    }
+   
+    
+ return 0;
+}
+
+string charArr2String(char* array){
+    int i = 0;
+    string sentence = "";
+    while(array[i] != '\0'){
+        sentence += array[i];
+        i++;
+    }
+    return sentence;
+}
+void startPrint(){
+    cout << "//----Welcome to the COMP database----\\\\" << endl;
+    cout << "This is the menu enter:" << endl;
+    cout << "1- To search a subject title using a key word" << endl;
+    cout << "2- To search a subject code by using a subject code" <<endl;
+    cout << "3- To search what a subject is a pre-requisite to" << endl;
+    cout << "4- To quit" << endl;
+}
+string userInput(){
+    // todo add data verification
+    char user[160];
+    cout<< "Please enter in a key word or phrase" << endl;
+    cin >> user;
+    string userIn;
+    userIn = charArr2String(user);
+    return userIn;
+}
+
+int dataValidation(int cont){
+    cin >> cont;
+    //TODO PREVENT USERS FROM CREATING AN INFINITE LOOP
+    while(cont != 1){
+        cout<< "please enter in one" << endl;
+        cin >> cont;
+    }
+    return cont;
+}
+void printData(Node *list){
+    list = list->reverse(list);
+    list = list->next;
+    cout <<"|"<< setw(12) << "CODE| " << "TITLE" << endl;
+    cout << "------------------------------------------------------------" << endl;
+    while(list->next != NULL){
+        cout << "|" <<setw(10)  << list->getData().subCode <<"|"  << list->getData().subTitle << endl;
+        cout << "Pre-Requisites:";
+        for(int re = 0; re<5; re++){//print all pre-req
+            cout <<"  "<< list->getData().preReq[re];
+        }
+        cout << endl << "Credits: " << list->getData().credit << endl;
+        cout << "Weight: " << list->getData().weight << endl;
+        cout <<"Lecture/On-site(hours):" << list->getData().lecture << endl;
+        cout << "Student Effort(hours): " << list->getData().tls << endl;
+        cout  << "Cumulative Assessement(%):" << list->getData().ca << endl;
+        cout << "Final Exam(%): " <<  list->getData().exam <<" | \n" << endl;
+        list = list->next;
+    }
+    //cout << "|" <<list->getData().subCode <<" | "<< list->getData().subTitle << " | "<< list->getData().preReq[0]<<" | " << list->getData().ca <<" | " <<  list->getData().exam <<" |\n\n" << endl;
+     cout << "|" <<setw(10)  << list->getData().subCode <<"|"  << list->getData().subTitle << endl;
+        cout << "Pre-Requisites:" << list->getData().preReq[0];
+        for(int re = 1; re<5; re++){//print all pre-req
+            cout <<"  "<< list->getData().preReq[re];
+        }
+        cout << endl << "Weight: " << list->getData().weight << endl;
+        cout <<"Lecture/On-site(hours):" << list->getData().lecture << endl;
+        cout << "Student Effort(hours): " << list->getData().tls << endl;
+        cout  << "Cumulative Assessement(%):" << list->getData().ca << endl;
+        cout << "Final Exam(%): " <<  list->getData().exam <<" | \n" << endl;
+}
+
+void searchSub(){
+    int found = 0;
+  //END TODO
     //get query input
     string query = userInput();
     cout << "\nSearching for " << query << endl;
@@ -38,28 +132,8 @@ int main(){
     Node *list = new Node();
     //check if the the Link list exists
     bool outer = false;
-/*//Sam's experiment:
-    cout << "Enter Sam Query: ";
-    char quero[70];
-    cin >> quero;
-    char *ptrQuero = quero;
-    char *ptrSub = 0;
 
-    for(int i = 45; i<56;i++){
-        Subject subject = p[i];
-        ptrSub = subject.subTitle;
-        cout << subject.subTitle << endl;
-        if (search.searchPattern2(ptrQuero,ptrSub) == true){
-            cout << subject.subTitle << endl;
-        }
-        else{
-            //cout << "search pattern fail";
-        }
-        //cout << "for looping" << i;
-    }
-    cout << "out of for loop";
- */
-    for(int i = 45; i<67; i++){
+    for(int i = 0; i<81; i++){
         //get subject at specific i index
         Subject subject = p[i];
         //turn the title into a string
@@ -86,52 +160,93 @@ int main(){
     if(outer == true){
         printData(list);
     }
- return 0;
 }
 
-string charArr2String(char* array){
-    int i = 0;
-    string sentence = "";
-    while(array[i] != '\0'){
-        sentence += array[i];
-        i++;
+void searchCode(){
+     int found = 0;
+    //END TODO
+    //get query input
+    string query = userInput();
+    cout << "\nSearching for: " << query << endl;
+    //initialize all the data for the query
+    Search search;
+    Subject info[85];
+    Subject *p = info;
+    cout << p[60].subTitle;
+    p = init_data();
+
+    //Create new Link list
+    Node *list = new Node();
+    //check if the the Link list exists
+    bool outer = false;
+    for(int i = 0; i<81; i++){
+
+        //get subject at specific i index
+        Subject subject = p[i];
+        //turn the title into a string
+        string data = charArr2String(p[i].subCode);
+        //see whether pattern occurs in the string
+        bool yn = search.searchPattern(query,data);
+        //if its true
+        if(yn){
+            //cout << p[i].exam << endl;
+            //add the specific subject to the link list
+           list = list->push(list, subject);
+           //ensure that we know the link list has objects inside of it
+           outer = true;
+           found += 1;
+        }
     }
-    return sentence;
-}
-void startPrint(){
-    cout << "//----Welcome to the COMP database----\\\\" << endl;
-    cout << "This is the menu enter:" << endl;
-    cout << "1- To search a subject title using a key word" << endl;
-    cout << "2- To search a subject code by using a subject code" <<endl;
-    cout << "4- To quit" << endl;
-}
-string userInput(){
-    // todo add data verification
-    char user[160];
-    cout<< "Please enter in a key word or phrase" << endl;
-    cin >> user;
-    string userIn;
-    userIn = charArr2String(user);
-    return userIn;
+    cout << "\n\nThere were " << found << " occurences found for the search term: " << query << "\n\n" << endl;
+    //if we know the link list has objects inside of it
+    if(outer == true){
+        printData(list);
+    }
+    
 }
 
-int dataValidation(int cont){
-    cin >> cont;
-    //TODO PREVENT USERS FROM CREATING AN INFINITE LOOP
-    while(cont != 1){
-        cout<< "please enter in one" << endl;
-        cin >> cont;
+void searchPre(){
+     int found = 0;
+    //END TODO
+    //get query input
+    string query = userInput();
+    cout << "\nSearching for: " << query << endl;
+    //initialize all the data for the query
+    Search search;
+    Subject info[85];
+    Subject *p = info;
+    cout << p[60].subTitle;
+    p = init_data();
+
+    //Create new Link list
+    Node *list = new Node();
+    //check if the the Link list exists
+    bool outer = false;
+    for(int i = 0; i<81; i++){
+
+        //get subject at specific i index
+        Subject subject = p[i];
+        //turn the title into a string
+        for(int j = 0; j < 5; j++){
+        string data = charArr2String(p[i].preReq[j]);
+        
+        //see whether pattern occurs in the string
+        bool yn = search.searchPattern(query,data);
+        //if its true
+            if(yn){
+                //cout << p[i].exam << endl;
+                //add the specific subject to the link list
+            list = list->push(list, subject);
+            //ensure that we know the link list has objects inside of it
+            outer = true;
+            found += 1;
+            }
+        }
     }
-    return cont;
-}
-void printData(Node *list){
-    list = list->reverse(list);
-    list = list->next;
-    cout << setw(10) << "| CODE " <<setw(40) << "| TITLE \t\t |Pre-Requisites\t | CA | Exam | " << endl;
-    cout << "------------------------------------------------------------" << endl;
-    while(list->next != NULL){
-        cout << "|" <<list->getData().subCode <<" | "<< list->getData().subTitle << " | "<< list->getData().preReq[0]<<"| " << list->getData().ca <<" | " <<  list->getData().exam <<" | \n" << endl;
-        list = list->next;
+    cout << "\n\nThere were " << found << " occurences found for the search term: " << query << "\n\n" << endl;
+    //if we know the link list has objects inside of it
+    if(outer == true){
+        printData(list);
     }
-    cout << "|" <<list->getData().subCode <<" | "<< list->getData().subTitle << " | "<< list->getData().preReq[0]<<" | " << list->getData().ca <<" | " <<  list->getData().exam <<" |\n\n" << endl;
+    
 }
